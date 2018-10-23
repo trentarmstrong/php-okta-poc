4b99e80d3b01c2fb17e7df150b188d31963384f0
Trent Armstrong <trenta@slalom.com>
2018-10-23 09:13:10 -0700

    separated test.php and updated startup script to clone the repo in place

---
+++ startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update the server and install Apache, PHP, and GIT.
+yum update -y
+yum install httpd24 php56 git -y
+
+# Start apache and set it to update on file changes
+service httpd start
+chkconfig httpd on
+
+# required COMPOSER_HOME dir to work because aws doesn't have a HOME var set during initialization
+export COMPOSER_HOME=/root
+
+# install composer at usr/bin/
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/bin/composer
+
+# pull the okta poc repo in /var/www/html
+cd /var/www/html
+git clone https://github.com/trentarmstrong/php-okta-poc .
